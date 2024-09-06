37998d5d9633d6a1afac4fbce23e28fe24f641ab
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-06 11:25:03 +0200

    Initial Commit

---
+++ NavigationStacksSwiftUI/NavigationStacksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStacksSwiftUIApp.swift
+//  NavigationStacksSwiftUI
+//
+//  Created by Manyuchi, Carrington C on 2024/09/06.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStacksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
